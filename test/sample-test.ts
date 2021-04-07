95c43df1b1ef63f9189bcb161a3f1565645f1cb9
Sebastian Goldberg <sebas.goldberg@gmail.com>
2021-04-07 19:38:55 +0000

    Added to test usage of fixture from deployments and usage of typechain.

--- test/sample-test.ts
+++ test/sample-test.ts
@@ -1,13 +1,15 @@
 import { expect } from "chai";
-import { ethers } from "hardhat";
+import { ethers, deployments } from "hardhat";
+import { Greeter } from "../frontend/src/hardhat/types/Greeter"
 
 describe("Greeter", function () {
   it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
 
-    await greeter.deployed();
-    expect(await greeter.greet()).to.equal("Hello, world!");
+    await deployments.fixture()
+
+    const greeter = (await ethers.getContract('Greeter')) as Greeter
+
+    expect(await greeter.greet()).to.equal("Hi!");
 
     await greeter.setGreeting("Hola, mundo!");
     expect(await greeter.greet()).to.equal("Hola, mundo!");
