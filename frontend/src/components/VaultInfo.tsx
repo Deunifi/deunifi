dd827f742a51bdb132c42dabf70776a0a68f195e
Sebastian Goldberg <sebas.goldberg@gmail.com>
2021-04-10 22:36:01 +0000

    - Getting collateral and debt for vault.

--- frontend/src/components/VaultInfo.tsx
+++ frontend/src/components/VaultInfo.tsx
@@ -1,4 +1,6 @@
 import { BigNumber } from '@ethersproject/bignumber';
+import { formatBytes32String } from '@ethersproject/strings';
+import { formatEther, parseUnits } from '@ethersproject/units';
 import React, { useEffect, useRef, useState } from 'react';
 import { useEffectAsync } from '../hooks/useEffectAsync';
 import { useContract } from './Deployments';
@@ -6,22 +8,39 @@ import { useVaultContext, VaultSelection } from './VaultSelection';
 
 interface Props { }
 
+const ONE_RAY = parseUnits('1',27)
+
 export const VaultInfo: React.FC<Props> = () => {
 
     const {vault} = useVaultContext()
+
     const manager = useContract('DssCdpManager')
+    const vat = useContract('Vat')
+
     const [urn, setUrn] = useState()
+    const [ink, setInk] = useState(BigNumber.from(0))
+    const [dart, setDart] = useState(BigNumber.from(0))
 
     useEffectAsync(async () => {
             
-        if (!vault || !manager){
+        if (!vault || !manager || !vat){
             setUrn(undefined)
+            setInk(BigNumber.from(0))
+            setDart(BigNumber.from(0))
             return
         }
 
-        const _urn = await manager.urns(BigNumber.from(vault.cdp));
+        const urn = await manager.urns(vault.cdp);
+        setUrn(urn)
 
-        setUrn(_urn)
+        const { rate } = await vat.ilks(formatBytes32String(vault.ilk))
+
+        const { ink, art }: { ink: BigNumber, art: BigNumber } = await vat.urns(formatBytes32String(vault.ilk), urn)
+
+        const dart = art.isZero() ? art : art.mul(rate).div(ONE_RAY).add(1)
+
+        setInk(ink)
+        setDart(dart)
 
     }, [vault, manager])
 
@@ -30,6 +49,8 @@ export const VaultInfo: React.FC<Props> = () => {
             <li>#{vault?.cdp.toString()}</li>
             <li>Ilk: {vault?.ilk}</li>
             <li>Urn: {urn}</li>
+            <li>Ink: {formatEther(ink)}</li>
+            <li>Dart: {formatEther(dart)}</li>
         </ul>
     )
 }
