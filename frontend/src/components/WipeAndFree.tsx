12a527d6ff31c0f27c30b1fbd6ee8fa3bf8aa39f
Sebastian Goldberg <sebas.goldberg@gmail.com>
2021-04-13 14:02:52 +0000

    - WipeAndFreeComponent:   - Added slippage tolerance.   - Added deadline.   - Added min amount to recieve for each token of pair.   - Added amount of pair tokens to recive.

--- frontend/src/components/WipeAndFree.tsx
+++ frontend/src/components/WipeAndFree.tsx
@@ -22,6 +22,7 @@ interface IWipeAndFreeParameters {
 
     slippageTolerance: BigNumber, // ratio with 6 decimals
     transactionDeadline: BigNumber, // minutes
+
 }
 
 const emptyWipeAndFreeParameters: IWipeAndFreeParameters = {
@@ -33,8 +34,8 @@ const emptyWipeAndFreeParameters: IWipeAndFreeParameters = {
     daiFromTokenA: BigNumber.from(0),
     daiFromTokenB: BigNumber.from(0),
 
-    slippageTolerance: BigNumber.from(0), // ratio with 6 decimals
-    transactionDeadline: BigNumber.from(0), // minutes
+    slippageTolerance: parseUnits('.01',6), // ratio with 6 decimals
+    transactionDeadline: BigNumber.from(120), // minutes
 }
 
 interface IWipeAndFreeForm {
@@ -47,7 +48,7 @@ interface IWipeAndFreeForm {
     daiFromTokenA: string,
     daiFromTokenB: string,
 
-    slippageTolerance: string, // ratio with 6 decimals
+    slippageTolerance: string, // percentage with 4 decimals
     transactionDeadline: string, // minutes
 }
 
@@ -60,8 +61,8 @@ const emptyWipeAndFreeForm: IWipeAndFreeForm = {
     daiFromTokenA: '',
     daiFromTokenB: '',
 
-    slippageTolerance: '',
-    transactionDeadline: '',
+    slippageTolerance: formatUnits(emptyWipeAndFreeParameters.slippageTolerance, 4),
+    transactionDeadline: emptyWipeAndFreeParameters.transactionDeadline.toString(),
 }
 
 export const getLoanFee = (amount: BigNumber) => amount.mul(9).div(10000)
@@ -73,7 +74,8 @@ export const parseBigNumber = (text:string, decimals=18) => text ? parseUnits(te
 interface IErrors {
     tooMuchDai?: string,
     tooMuchCollateralToFree?: string
-    notEnoughCollateralToCoverDai?: string
+    notEnoughCollateralToCoverDai?: string,
+    notEnoughCollateralToFree?: string,
 }
 
 export const WipeAndFree: React.FC<Props> = ({ children }) => {
@@ -124,6 +126,9 @@ export const WipeAndFree: React.FC<Props> = ({ children }) => {
     const router02 = useContract('UniswapV2Router02')
     const dai = useContract('Dai')
 
+    const [token0MinAmountToRecieve, setToken0MinAmountToRecieve] = useState(BigNumber.from(0))
+    const [token1MinAmountToRecieve, setToken1MinAmountToRecieve] = useState(BigNumber.from(0))
+
     useEffectAsync(async () => {
         
         const lastDaiLoanFees = getLoanFee(params.daiFromFlashLoan)
@@ -153,15 +158,18 @@ export const WipeAndFree: React.FC<Props> = ({ children }) => {
          * TokenA -> DAI TokenA: 1) getAmountsIn(DAI TokenA) to obtain TokenA
          * TokenB -> DAI TokenB: 2) getAmountsIn(DAI TokenB) to obtain TokenB
          */
-        const minCollateralToRemove = await ((async () =>{
+        const [minCollateralToRemove, token0AmountForDai, token1AmountForDai, pairToken0Balance, pairToken1Balance, pairTotalSupply ] = await ((async () =>{
 
             const { univ2Pair, token0, token1 } = vaultInfo.ilkInfo
 
             if (!univ2Pair || !token0 || !token1 || !dai || !router02){
-                return BigNumber.from(0)
+                return [BigNumber.from(0), BigNumber.from(0), BigNumber.from(0), BigNumber.from(0), BigNumber.from(0), BigNumber.from(0),]
             }
 
             const pairTotalSupply: BigNumber = await univ2Pair.totalSupply()
+            const pairToken0Balance: BigNumber = await token0.contract.balanceOf(univ2Pair.address)
+            const pairToken1Balance: BigNumber = await token1.contract.balanceOf(univ2Pair.address)
+    
             
             // TODO In case of dai, should be the same amount.
             const token0AmountForDai: BigNumber = params.daiFromTokenA.isZero() ?
@@ -174,9 +182,6 @@ export const WipeAndFree: React.FC<Props> = ({ children }) => {
                 : (await router02.getAmountsIn(
                     params.daiFromTokenB, [token1.contract.address, dai.address]))[0];
 
-            const pairToken0Balance: BigNumber = await token0.contract.balanceOf(univ2Pair.address)
-            const pairToken1Balance: BigNumber = await token1.contract.balanceOf(univ2Pair.address)
-    
             const minLiquidityToRemoveForToken0 = token0AmountForDai
                 .mul(pairTotalSupply)
                 .div(pairToken0Balance)
@@ -187,21 +192,40 @@ export const WipeAndFree: React.FC<Props> = ({ children }) => {
             const minLiquidityToRemove = minLiquidityToRemoveForToken0.gt(minLiquidityToRemoveForToken1) ?
                 minLiquidityToRemoveForToken0
                 : minLiquidityToRemoveForToken1
-
-            return minLiquidityToRemove
+            
+            return [minLiquidityToRemove, token0AmountForDai, token1AmountForDai, pairToken0Balance, pairToken1Balance, pairTotalSupply]
 
         })())
 
-        if (params.collateralToUseToPayFlashLoan.lt(minCollateralToRemove))
-            errors.notEnoughCollateralToCoverDai = `The amount to remove from pool it is not enough. Minimal amount is ${formatEther(minCollateralToRemove)}.`
+        const token0ToRecieve = pairTotalSupply.isZero()? 
+            BigNumber.from(0) : 
+            params.collateralToUseToPayFlashLoan.mul(pairToken0Balance).div(pairTotalSupply)
+        const token1ToRecieve = pairTotalSupply.isZero()? 
+            BigNumber.from(0) : 
+            params.collateralToUseToPayFlashLoan.mul(pairToken1Balance).div(pairTotalSupply)
+
+        setToken0MinAmountToRecieve(token0ToRecieve.sub(token0AmountForDai))
+        setToken1MinAmountToRecieve(token1ToRecieve.sub(token1AmountForDai))
+
+        const SLIPPAGE_TOLERANCE_UNIT = parseUnits('1', 6)
+
+        const minCollateralToRemoveWithTolerance = minCollateralToRemove
+            .mul(SLIPPAGE_TOLERANCE_UNIT.add(params.slippageTolerance))
+            .div(SLIPPAGE_TOLERANCE_UNIT)
+
+        if (params.collateralToUseToPayFlashLoan.lt(minCollateralToRemoveWithTolerance))
+            errors.notEnoughCollateralToCoverDai = `The amount to remove from pool it is not enough. Minimal amount is ${formatEther(minCollateralToRemoveWithTolerance)}.`
+
+        if (params.collateralToFree.lt(params.collateralToUseToPayFlashLoan))
+            errors.notEnoughCollateralToFree = `The collateral amount to free from vault it is not enough. Minimal amount is ${formatEther(params.collateralToUseToPayFlashLoan)}.`
 
         setErrors(errors)
 
     }, [params])
 
-    const onChangeBigNumber = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const onChangeBigNumber = (e: React.ChangeEvent<HTMLInputElement>, decimals: number=18) => {
         try {
-            const value = parseBigNumber(e.target.value)
+            const value = parseBigNumber(e.target.value, decimals)
             setParams({...params, [e.target.name]: value})
         } catch (error) {
             
@@ -211,51 +235,78 @@ export const WipeAndFree: React.FC<Props> = ({ children }) => {
 
     return (
         <form>
-            <br></br>
-            <label>
-                DAI From Signer:
-                <input type="number" value={form.daiFromSigner} name="daiFromSigner" onChange={(e) => onChangeBigNumber(e)}/>
-            </label>
-            <br></br>
-            <label>
-                DAI From Flash Loan:
-                <input type="number" value={form.daiFromFlashLoan} name="daiFromFlashLoan" onChange={(e) => onChangeBigNumber(e)}/>
-            </label>
-            {errors.tooMuchDai ? <p>{errors.tooMuchDai}</p> : ''}
-            {daiLoanFees.isZero() ? '' : <p>Flash Loan Fees (0.09%): {formatEther(daiLoanFees)} DAI</p>}
-            {daiServiceFee.isZero() ? '' : <p>Service Fee (0.03%): {formatEther(daiServiceFee)} DAI</p>}
-            {daiLoanPlusFees.isZero() ? '' : <p>Total Dai to get from collateral: {formatEther(daiLoanPlusFees)} DAI</p>}
+            <p>
+                <label>
+                    DAI From Signer:
+                    <input type="number" value={form.daiFromSigner} name="daiFromSigner" onChange={(e) => onChangeBigNumber(e)}/>
+                </label>
+                <br></br>
+                <label>
+                    DAI From Flash Loan:
+                    <input type="number" value={form.daiFromFlashLoan} name="daiFromFlashLoan" onChange={(e) => onChangeBigNumber(e)}/>
+                </label>
+            </p>
 
-            <br></br>
-            <label>
-                DAI Covered With {vaultInfo.ilkInfo.token0?.symbol}:
-                <input type="number" value={form.daiFromTokenA} name="daiFromSigner" onChange={ (e) => daiFromTokenAChange(e) }/>
-            </label>
-            <br></br>
-            <label>
-                DAI Covered With {vaultInfo.ilkInfo.token1?.symbol}:
-                <input type="number" value={form.daiFromTokenB} name="daiFromFlashLoan"  onChange={ (e) => daiFromTokenBChange(e) }/>
-            </label>
-            <br></br>
+            <p>
+                {errors.tooMuchDai ? <div>{errors.tooMuchDai}<br></br></div> : ''}
+                {daiLoanFees.isZero() ? '' : <div>Flash Loan Fees (0.09%): {formatEther(daiLoanFees)} DAI<br></br></div>}
+                {daiServiceFee.isZero() ? '' : <div>Service Fee (0.03%): {formatEther(daiServiceFee)} DAI<br></br></div>}
+                {daiLoanPlusFees.isZero() ? '' : <div>Total Dai to get from collateral: {formatEther(daiLoanPlusFees)} DAI<br></br></div>}
+            </p>
 
-
-            <br></br>
-            <label>
-                {vaultInfo.ilkInfo.symbol} To Remove From Pool:
-                <input type="number" value={form.collateralToUseToPayFlashLoan} name="collateralToUseToPayFlashLoan" onChange={(e) => onChangeBigNumber(e)}/>
-                {errors.notEnoughCollateralToCoverDai ? <p>{errors.notEnoughCollateralToCoverDai}</p> : ''}
-            </label>
-            <br></br>
-            <label>
-                {vaultInfo.ilkInfo.symbol} To Free From Vault:
-                <input type="number" value={form.collateralToFree} name="collateralToFree" onChange={(e) => onChangeBigNumber(e)}/>
-                {errors.tooMuchCollateralToFree ? <p>{errors.tooMuchCollateralToFree}</p> : ''}
-            </label>
-            <br></br>
+            <p>
+                <label>
+                    DAI Covered With {vaultInfo.ilkInfo.token0?.symbol}:
+                    <input type="number" value={form.daiFromTokenA} name="daiFromSigner" onChange={ (e) => daiFromTokenAChange(e) }/>
+                </label>
+                <br></br>
+                <label>
+                    DAI Covered With {vaultInfo.ilkInfo.token1?.symbol}:
+                    <input type="number" value={form.daiFromTokenB} name="daiFromFlashLoan"  onChange={ (e) => daiFromTokenBChange(e) }/>
+                </label>
+            </p>
 
 
-            <br></br>
-            <br></br>
+            <p>
+                <label>
+                    {vaultInfo.ilkInfo.symbol} To Remove From Pool:
+                    <input type="number" value={form.collateralToUseToPayFlashLoan} name="collateralToUseToPayFlashLoan" onChange={(e) => onChangeBigNumber(e)}/>
+                    {errors.notEnoughCollateralToCoverDai ? <p>{errors.notEnoughCollateralToCoverDai}</p> : ''}
+                    {errors.notEnoughCollateralToCoverDai? 
+                        '': 
+                        <div>
+                            <div>Min Amount of {vaultInfo.ilkInfo.token0?.symbol} to recieve: {formatUnits(token0MinAmountToRecieve,vaultInfo.ilkInfo.token0?.decimals || 18)} {vaultInfo.ilkInfo.token0?.symbol}<br></br></div>
+                            <div>Min Amount of {vaultInfo.ilkInfo.token1?.symbol} to recieve: {formatUnits(token1MinAmountToRecieve,vaultInfo.ilkInfo.token1?.decimals || 18)} {vaultInfo.ilkInfo.token1?.symbol}<br></br></div>
+                        </div>}
+                </label>
+
+                <br></br>
+                <label>
+                    {vaultInfo.ilkInfo.symbol} To Free From Vault:
+                    <input type="number" value={form.collateralToFree} name="collateralToFree" onChange={(e) => onChangeBigNumber(e)}/>
+                    {errors.tooMuchCollateralToFree ? <p>{errors.tooMuchCollateralToFree}</p> : ''}
+                    {errors.notEnoughCollateralToFree ? <p>{errors.notEnoughCollateralToFree}</p> : ''}
+                    {(errors.tooMuchCollateralToFree || errors.notEnoughCollateralToFree) ? 
+                        '': 
+                        <div>
+                            <div>Amount of {vaultInfo.ilkInfo.symbol} to recieve: {formatUnits(params.collateralToFree.sub(params.collateralToUseToPayFlashLoan), vaultInfo.ilkInfo.dec)} {vaultInfo.ilkInfo.symbol}<br></br></div>
+                        </div>}
+
+                </label>
+            </p>
+
+            <p>
+                <label>
+                    Slippage Tolerance (%):
+                    <input type="number" value={form.slippageTolerance} name="slippageTolerance" onChange={(e) => onChangeBigNumber(e,4)}/>
+                </label>
+                <br></br>
+                <label>
+                    Transaction Deadline (minutes):
+                    <input type="number" value={form.transactionDeadline} name="transactionDeadline" onChange={(e) => onChangeBigNumber(e,0)}/>
+                </label>
+            </p>
+
             <input type="submit" value="Submit" />
         </form>
 
